d0c2776c33e017ec4718a15e9a6cacddd1437f56
Juhee Kim <julieK1217@gmail.com>
2019-11-23 01:49:03 -0600

    something

--- js/languageoption.js
+++ js/languageoption.js
@@ -1,21 +1,6 @@
-/*
-var flags = document.getElementsByClassName('flag_link');
-
-
-Array.prototype.forEach.call(flags, function(e){
-  e.addEventListener('click', function(){
-    var lang = e.getAttribute('data-lang'); 
-    var languageSelect = document.querySelector("select.goog-te-combo");
-    languageSelect.value = lang; 
-    languageSelect.dispatchEvent(new Event("change"));
-  }); 
-});
-
-*/
-
 
     function googleTranslateElementInit() {
-      new google.translate.TranslateElement({pageLanguage: 'pt', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+      new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
     }
 
 	function triggerHtmlEvent(element, eventName) {
